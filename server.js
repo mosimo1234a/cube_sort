90384887398185450c2b84f69380fb051c0a55db
mosimo1234a <mosimo1234a@gmail.com>
2026-07-31 14:11:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const rooms = {};
 
-// 알고리즘 퍼즐 정답 생성기
+// 알고리즘 퍼즐 생성 (버블, 선택, 삽입 중 랜덤)
 function generatePuzzle() {
     const types = ['bubble', 'selection', 'insertion'];
     const type = types[Math.floor(Math.random() * types.length)];
     
-    // 무작위 숫자 배열 (정렬해야 하는 대상)
     let numbers = [
         Math.floor(Math.random() * 9) + 1,
         Math.floor(Math.random() * 9) + 1,
@@ -41,7 +40,7 @@ io.on('connection', (socket) => {
         if (!rooms[socket.roomId]) {
             rooms[socket.roomId] = {
                 players: {},
-                switches: { 1: false, 2: false }, // 협동 스위치 상태
+                switches: { 1: false, 2: false },
                 puzzle: generatePuzzle(),
                 isSolved: false
             };
@@ -50,7 +49,7 @@ io.on('connection', (socket) => {
         rooms[socket.roomId].players[socket.id] = {
             id: socket.id,
             nickname: nickname || '모험가',
-            color: color || '#ff5722',
+            color: color || '#e67e22',
             x: 0, y: 0, z: 0, rotationY: 0
         };
 
@@ -77,7 +76,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 협동 스위치 밟기 상태 동기화
     socket.on('updateSwitch', (data) => {
         const roomId = socket.roomId;
         if (roomId && rooms[roomId]) {
@@ -86,7 +84,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 정렬 퍼즐 제출 검증
     socket.on('submitPuzzle', (userAnswer) => {
         const roomId = socket.roomId;
         if (roomId && rooms[roomId]) {
