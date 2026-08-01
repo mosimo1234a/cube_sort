54e259b25eb08ba00803b9e22a54c535fc88839f
mosimo1234a <mosimo1234a@gmail.com>
2026-08-01 10:31:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// public 폴더의 정적 파일(index.html 등) 서비스
 app.use(express.static('public'));
 
-const players = {}; // 접속한 플레이어들의 정보 저장
+const players = {};
 
 io.on('connection', (socket) => {
     console.log(`플레이어 접속: ${socket.id}`);
 
-    // 방 입장 처리
     socket.on('joinRoom', (data) => {
         const { roomId, nickname, color } = data;
         socket.join(roomId);
@@ -33,7 +31,6 @@ io.on('connection', (socket) => {
             score: 0
         };
 
-        // 해당 방의 기존 플레이어 목록 전송
         const roomPlayers = {};
         Object.keys(players).forEach(id => {
             if (players[id].roomId === roomId) {
@@ -41,12 +38,9 @@ io.on('connection', (socket) => {
             }
         });
         socket.emit('currentPlayers', roomPlayers);
-
-        // 다른 방원들에게 새 플레이어 입장 알림
         socket.to(roomId).emit('newPlayer', players[socket.id]);
     });
 
-    // 위치 및 회전 동기화
     socket.on('playerUpdate', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
@@ -64,7 +58,6 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 총알 발사 브로드캐스트
     socket.on('shoot', (data) => {
         if (players[socket.id]) {
             socket.to(socket.roomId).emit('playerShooting', {
@@ -79,10 +72,9 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 피격 판정 및 점수 처리
     socket.on('hitPlayer', (targetId) => {
         if (players[targetId] && players[socket.id]) {
-            players[targetId].hp -= 25; // 총알 당 25 데미지
+            players[targetId].hp -= 25; // 4발 맞으면 사망
 
             if (players[targetId].hp <= 0) {
                 players[targetId].hp = 100; // 부활
@@ -91,7 +83,6 @@ io.on('connection', (socket) => {
                 players[socket.id].score += 1; // 맞춘 사람 점수 획득
             }
 
-            // 방 안의 모든 사람에게 체력/점수 업데이트 전송
             const roomPlayers = {};
             Object.keys(players).forEach(id => {
                 if (players[id].roomId === socket.roomId) {
@@ -99,10 +90,12 @@ io.on('connection', (socket) => {
                 }
             });
             io.to(socket.roomId).emit('updateStats', roomPlayers);
+            
+            // 맞은 플레이어에게 피격 신호 전송
+            io.to(targetId).emit('damaged');
         }
     });
 
-    // 접속 종료 처리
     socket.on('disconnect', () => {
         console.log(`플레이어 퇴장: ${socket.id}`);
         if (players[socket.id]) {
